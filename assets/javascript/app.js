90e0aca562d1fcb7e50b076b5228b7f85a0098b3
Zaya <zayazaw67@gmail.com>
2019-09-12 17:51:49 -0500

    added elements in html, added loops to display questions in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,50 +5,68 @@ $("#start").on("click", startGame)
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var timerOn = false
-var number = 120;
+// var timerOn = false
+var number = 20;
 var intervalId;
 var userAnswer;
+// var questionIndex;
+
+
 
 // var questionCounter = 0;
-var questions = ["hello", "test", "bye", "hi"
-    // {
-    //     question: "What year did World War I begin?",
-    //     answers: [
-    //         "1916",
-    //         "1914",
-    //         "1919",
-    //         "1923",
-    //     ],
-    //     correctAnwer: "1914"
-    // },
-    // {
-    //     question: "Where was John F. Kennedy assassinated?",
-    //     answers: [
-    //         "Texas",
-    //         "New York",
-    //         "Louisiana",
-    //         "Maine",
-    //     ],
-    //     correctAnwer: "Texas"
-    // },
-    // {
-    //     question: "When did Hawaii become apart of the United States?",
-    //     answers: [
-    //         "1979",
-    //         "1984",
-    //         "1989",
-    //         "1904",
-    //     ],
-    //     correctAnwer: "1989"
-    // },
-]
+var questions = [
+    {
+        question: "What year did World War I begin?",
+        answers: [
+            "1916",
+            "1914",
+            "1919",
+            "1923",
+        ],
+        correctAnwer: "1914"
+    },
+    {
+        question: "Where was John F. Kennedy assassinated?",
+        answers: [
+            "Texas",
+            "New York",
+            "Louisiana",
+            "Maine",
+        ],
+        correctAnwer: "Texas"
+    },
+    {
+        question: "When did Hawaii become apart of the United States?",
+        answers: [
+            "1979",
+            "1984",
+            "1989",
+            "1904",
+        ],
+        correctAnwer: "1989"
+    },
+];
+
+for (var i = 0; i < questions.length; i++) {
+    var newQuestions = $('<div>').addClass('triviaForm');
+    newQuestions.append('<p>' + questions[i].question + '</p>');
+    for (var j = 0; j < questions[i].answers.length; j++) {
+        var answers = $('<p>');
+        answers.text(questions[i].answers[j]);
+        newQuestions.append(answers);
+    };
+
+    $('.question').append(newQuestions);
+    console.log(newQuestions)
+};
 
 
 
 function startGame() {
     $("#start").hide()
     $(".questions").show()
+    // displayQuestions()
+
     intervalId = setInterval(decrement, 1000);
     // if (userAnswer === correctAnswer) {
     //     correct++
@@ -73,10 +91,21 @@ function stop() {
     clearInterval(intervalId);
 }
 
-function displayQuestions() {
-    for (var i = 0; i < questions.length; i++) {
-        $(".questions").appendTo("<br><hr>" + questions)
-    }
-}
+// function displayQuestions() {
+//     for (var i = 0; i < questions.length; i++) {
+//         $(".questions").appendTo("<br><hr>" + questions.question)
+//     }
+// }
+// for (i = 0; i < questions.length; i++) {
+//     $(".questions").text(questions[i].question);
 
-$(".questions").text(questions) 
+
+    // $(".answers").text(questions[i].answers);
+
+    // console.log(questions[i])
+    // $(".questions").text(questions[i].answers[i]);
+
+    // for (i = 0; i < answers.length; i++); {
+    // }
+// }
+
